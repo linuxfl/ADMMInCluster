2b9105ade429277f171420d70743dcaee4e48899
Ling.Fang <androidfl@shu.edu.cn>
2016-06-16 13:04:19 +0800

    align

--- src/LinearRegression.cpp
+++ src/LinearRegression.cpp
@@ -14,34 +14,36 @@
 
 namespace LR {
 
-    // Constructor
-    LinearRegression::LinearRegression(): thread_counter_(0) {
-        // timer
-        initT_ = boost::posix_time::microsec_clock::local_time();
-
-        /* context */
-        lda::Context & context = lda::Context::get_instance();
-
-        // objective function parameters
-        feature = context.get_int32("feature");
+	// Constructor
+	LinearRegression::LinearRegression(): thread_counter_(0) {
+		// timer
+		initT_ = boost::posix_time::microsec_clock::local_time();
+		
+		/* context */
+		lda::Context & context = lda::Context::get_instance();
+		
+		// objective function parameters
+		feature = context.get_int32("feature");
 		row = context.get_int32("row");
 		
-        // petuum parameters
-        client_id_ = context.get_int32("client_id");
-        num_clients_ = context.get_int32("num_clients");
-        num_worker_threads_ = context.get_int32("num_worker_threads");
+		// petuum parameters
+		client_id_ = context.get_int32("client_id");
+		num_clients_ = context.get_int32("num_clients");
+		num_worker_threads_ = context.get_int32("num_worker_threads");
+		
+		// optimization parameters
+		num_epochs_ = context.get_int32("num_epochs");
+		
+		//ADMM parameters
+		rho = context.get_double("rho");
+		lambda = context.get_double("lambda");
+		errorthreshold = context.get_double("errorthreshold");
+		data_file = context.get_string("data_file");
+		
+		LOG(INFO) <<"row: "<<row<< " feature: " << feature << " rho: "<<rho << " lambda: "
+		<< lambda <<" errorthreshold: " << errorthreshold;
+}
 
-        // optimization parameters
-        num_epochs_ = context.get_int32("num_epochs");
-        LOG(INFO) <<"row: "<<row<< "feature: " << feature;
-        
-        //ADMM parameters
-        rho = context.get_double("rho");
-        lambda = context.get_double("lambda");
-        errorthreshold = context.get_double("errorthreshold");
-        data_file = context.get_string("data_file");
-    }
-    
 	void LinearRegression::soft_threshold(Eigen::VectorXf src,Eigen::VectorXf &des,float t){
 		for(int i =0; i < feature;i++){
 			if(src(i) > t)
@@ -53,37 +55,34 @@ namespace LR {
 		}
 	}
 	
-    // linearRegression
-    void LinearRegression::Start() {
-        // thread id on a client
-        int thread_id = thread_counter_++;
-        petuum::PSTableGroup::RegisterThread();
-        LOG(INFO) << "client " << client_id_ << ", thread " 
-            << thread_id << " registers!";
-
-        // Get w table
-        petuum::Table<float> w_table = 
-            petuum::PSTableGroup::GetTableOrDie<float>(0);
-        //register row
-        if(thread_id == 0)
-			w_table.GetAsyncForced(0);
+	// linearRegression
+	void LinearRegression::Start() {
+		// thread id on a client
+		int thread_id = thread_counter_++;
+		petuum::PSTableGroup::RegisterThread();
+		LOG(INFO) << "client " << client_id_ << ", thread " 
+			<< thread_id << " registers!";
 			
-        petuum::PSTableGroup::GlobalBarrier();
-
-        float temp,error;
-        std::string A_data_file,b_data_file,sol_data_file;
-        
-        char Adir[100],bdir[100];
-        std::vector<float> w_cache;
-        
-        sprintf(Adir,"A%d.dat",client_id_*num_worker_threads_+thread_id);
-        sprintf(bdir,"b%d.dat",client_id_*num_worker_threads_+thread_id);
+		// Get w table
+		petuum::Table<float> w_table = petuum::PSTableGroup::GetTableOrDie<float>(0);
+		//register row
+		if(thread_id == 0)
+			w_table.GetAsyncForced(0);
+		petuum::PSTableGroup::GlobalBarrier();
+		float temp,error;
+		std::string A_data_file,b_data_file,sol_data_file;
+		
+		char Adir[100],bdir[100];
+		std::vector<float> w_cache;
+		
+		sprintf(Adir,"A%d.dat",client_id_*num_worker_threads_+thread_id);
+		sprintf(bdir,"b%d.dat",client_id_*num_worker_threads_+thread_id);
 		
 		A_data_file = data_file + Adir;
 		b_data_file = data_file + bdir;
 		sol_data_file = data_file + "solution.dat";
 		
-        FILE *fpA = fopen(A_data_file.c_str(),"r");
+		FILE *fpA = fopen(A_data_file.c_str(),"r");
 		FILE *fpb = fopen(b_data_file.c_str(),"r");
 		FILE *fps = fopen(sol_data_file.c_str(),"r");
 		
@@ -91,49 +90,48 @@ namespace LR {
 			LOG(INFO) << "open the source file error!!!";
 		}
 		
-        //source data
-        Eigen::MatrixXf A(row,feature);
-        Eigen::VectorXf b(row);
-        Eigen::VectorXf s(feature);
-        Eigen::MatrixXf lemon(feature,feature);
-        Eigen::MatrixXf lemonI(feature,feature);
-        Eigen::MatrixXf identity(feature,feature);
+		//source data
+		Eigen::MatrixXf A(row,feature);
+		Eigen::VectorXf b(row);
+		Eigen::VectorXf s(feature);
+		Eigen::MatrixXf lemon(feature,feature);
+		Eigen::MatrixXf lemonI(feature,feature);
+		Eigen::MatrixXf identity(feature,feature);
 		Eigen::VectorXf result(feature);
 		Eigen::VectorXf obj(row);
 		
-        //parameters
-        Eigen::VectorXf x(feature);
-        Eigen::VectorXf x_diff(feature);
-        Eigen::VectorXf z(feature);
-        Eigen::VectorXf y(feature);
-        Eigen::VectorXf w(feature);
-        Eigen::VectorXf w_old(feature);
-        Eigen::VectorXf w_diff(feature);
-
-        x.setZero();
-        z.setZero();
-        y.setZero();
-        w.setZero();
-        w_old.setZero();
-        w_diff.setZero();
-
-        //init the z_table
-        if(thread_id == 0 && client_id_ == 0){
+		//parameters
+		Eigen::VectorXf x(feature);
+		Eigen::VectorXf x_diff(feature);
+		Eigen::VectorXf z(feature);
+		Eigen::VectorXf y(feature);
+		Eigen::VectorXf w(feature);
+		Eigen::VectorXf w_old(feature);
+		Eigen::VectorXf w_diff(feature);
+		
+		x.setZero();
+		z.setZero();
+		y.setZero();
+		w.setZero();
+		w_old.setZero();
+		w_diff.setZero();
+		
+		//init the z_table
+		if(thread_id == 0 && client_id_ == 0){
 			petuum::UpdateBatch<float> w_update;
 			for(int i = 0;i < feature;i++){
 				w_update.Update(i,w(i));
 			}
 			w_table.BatchInc(0, w_update);
 		}
-
-        //load data from file
-        for(int i = 0;i < row;i++){
+		//load data from file
+		for(int i = 0;i < row;i++){
 			for(int j=0;j < feature;j++){
 				fscanf(fpA,"%f",&temp);
 				A(i,j) = temp;
 			}
 		}
-        for(int i = 0;i < row;i++){
+		for(int i = 0;i < row;i++){
 			fscanf(fpb,"%f",&temp);
 			b(i) = temp;
 		}
@@ -149,7 +147,7 @@ namespace LR {
 		petuum::PSTableGroup::GlobalBarrier();
 		
 		//begin to iteration
-        for(int iter = 0 ;iter < num_epochs_;iter++){
+		for(int iter = 0 ;iter < num_epochs_;iter++){
 			//get w from server
 			if(iter != 0){
 				petuum::RowAccessor row_acc;
@@ -163,7 +161,7 @@ namespace LR {
 			//update z with 2-norm
 			//z = (1.0/(rho + 2*lambda)) * w;
 			soft_threshold((1.0/rho * w),z,lambda/rho);
-            //update x
+			//update x
 			x = lemonI * (A.transpose() * b + rho * z - y);
 			//if(thread_id == 0 && client_id_ == 0){
 				//LOG(INFO) << "client_id : "<< client_id_ <<"thread_id : "<< thread_id;
@@ -211,25 +209,24 @@ namespace LR {
 			
 			//obj value
 			obj = A * x - b;
-			
+			obj_real = A * s - b;
 			if(thread_id == 0 && client_id_ == 0)
 				LOG(INFO) << "iter: " << iter << ", client " 
-                        << client_id_ << ", thread " << thread_id <<
-                        " primal error: " << error << " object value: "<< obj.squaredNorm();
-                        
-            if(error < errorthreshold)
-            {
+					<< client_id_ << ", thread " << thread_id <<
+						" primal error: " << error << " object value: "<< obj.squaredNorm();
+			if(error < errorthreshold)
+			{
 				boost::posix_time::time_duration runTime = 
-                    boost::posix_time::microsec_clock::local_time() - initT_;
-                LOG(INFO) << "Elapsed time is: "<< runTime.total_milliseconds() << " ms.";
+					boost::posix_time::microsec_clock::local_time() - initT_;
+				LOG(INFO) << "Elapsed time is: "<< runTime.total_milliseconds() << " ms.";
 				return;
 			}
 		}
 		boost::posix_time::time_duration runTime = 
-                    boost::posix_time::microsec_clock::local_time() - initT_;
-                LOG(INFO) << "Elapsed time is: "<< runTime.total_milliseconds() << " ms.";
+			boost::posix_time::microsec_clock::local_time() - initT_;
+		LOG(INFO) << "Elapsed time is: "<< runTime.total_milliseconds() << " ms.";
 	}
-
-    LinearRegression::~LinearRegression() {
-    }
+	
+	LinearRegression::~LinearRegression() {
+	}
 } // namespace LR
