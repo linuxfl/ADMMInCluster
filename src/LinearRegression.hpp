6a1e8ddd86060209d95d78a0889384c5b6cffbcd
Ling.Fang <androidfl@shu.edu.cn>
2016-06-24 09:27:35 +0800

    align

--- src/LinearRegression.hpp
+++ src/LinearRegression.hpp
@@ -8,36 +8,34 @@
 
 namespace LR {
 class LinearRegression {
-    public:
-        LinearRegression();
-        ~LinearRegression();
-        void Start();
-        void soft_threshold(Eigen::VectorXf src,Eigen::VectorXf &des,float t);
-    private:
-        std::atomic<int> thread_counter_;
-
-        // petuum parameters
-        int client_id_, num_clients_, num_worker_threads_;
-        
-        // objective function parameters
-        int rank_;
-
-        // evaluate parameters
-        int num_epochs_;
-    
-        // timer
-        boost::posix_time::ptime initT_;
-		
-		//data
-		int row,feature;
-		
-		//ADMM parameter
-		float rho;
-		float lambda;
-		float errorthreshold;
-		
-		//data file
-		std::string data_file;
+	public:
+			LinearRegression();
+			~LinearRegression();
+			void Start();
+			void soft_threshold(Eigen::VectorXf src,Eigen::VectorXf &des,float t);
+	private:
+			std::atomic<int> thread_counter_;
+			
+			// petuum parameters
+			int client_id_, num_clients_, num_worker_threads_;
+			int table_staleness_;
+			
+			// evaluate parameters
+			int num_epochs_;
+			
+			// timer
+			boost::posix_time::ptime initT_;
+			//data
+			int row,feature;
+			
+			//ADMM parameter
+			float rho;
+			float lambda;
+			float errorthreshold;
+			
+			//data file
+			std::string data_dir;
+			std::string output_dir;
 		
 };
 }; // namespace LR
